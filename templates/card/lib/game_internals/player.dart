d875abbbbdbf1b0ce307faa65bbe67c9f9c2de32
tf-cnaughton <cnaughton@tracefirst.com>
2025-03-17 14:33:44 +0000

    Add add card method

--- templates/card/lib/game_internals/player.dart
+++ templates/card/lib/game_internals/player.dart
@@ -3,13 +3,18 @@ import 'package:flutter/foundation.dart';
 import 'playing_card.dart';
 
 class Player extends ChangeNotifier {
-  static const maxCards = 7;
+  static const maxCards = 2;
 
   final List<PlayingCard> hand =
       List.generate(maxCards, (index) => PlayingCard.random());
 
   void removeCard(PlayingCard card) {
     hand.remove(card);
+    notifyListeners();
+  }
+
+  void addCard() {
+    hand.add(PlayingCard.random());
     notifyListeners();
   }
 }
