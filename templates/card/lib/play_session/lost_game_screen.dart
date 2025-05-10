736b091360e8c53521d2b91d62ee0d0e95e9535f
tf-cnaughton <cnaughton@tracefirst.com>
2025-05-10 19:37:01 +0100

    Resolve lost screen timestamp bug

--- templates/card/lib/play_session/lost_game_screen.dart
+++ templates/card/lib/play_session/lost_game_screen.dart
@@ -1,9 +1,11 @@
 import 'package:card/game_internals/score.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LostGameScreen extends StatelessWidget {
   final Score score;
+
   const LostGameScreen({super.key, required this.score});
 
   @override
@@ -20,20 +22,34 @@ class LostGameScreen extends StatelessWidget {
               'You Lost!',
               style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
             ),
-                       const SizedBox(height: 20),
+            const SizedBox(height: 20),
             Center(
               child: Text(
                 'Score: ${score.score}\n'
                 'Name: ${score.playerName}',
                 style: const TextStyle(
-                    fontFamily: 'Roboto', fontSize: 20),
+                  fontFamily: 'Roboto',
+                  fontSize: 20,
+                ),
               ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                GoRouter.of(context)
-                    .go('/menu'); // Navigate back to the main menu
+              onPressed: () async {
+                // Add a new record to Firestore
+                try {
+                  await FirebaseFirestore.instance.collection('game_results').add({
+                    'playerName': score.playerName,
+                    'score': score.score,
+                    'timestamp': FieldValue.serverTimestamp(), // Optional: Add a timestamp
+                  });
+                  print('Record added to Firestore');
+                } catch (e) {
+                  print('Error adding record to Firestore: $e');
+                }
+
+                // Navigate back to the main menu
+                GoRouter.of(context).go('/menu');
               },
               child: const Text('Back to Menu'),
             ),
